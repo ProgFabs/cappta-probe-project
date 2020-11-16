78cbb7fb1a5d91d339a97ae99463d7be2d9a9183
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2020-11-16 19:38:19 -0300

    Refactored ProbeController POST method.

--- API/Controllers/ProbeController.cs
+++ API/Controllers/ProbeController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Domain;
 using Application.Probes;
 using Newtonsoft.Json.Linq;
 
@@ -14,29 +13,8 @@ namespace API.Controllers
         [HttpPost]
         public List<string> Post([FromBody] JObject probeLaunchDTO) 
         {
-            var probeLaunchJson = JObject.Parse(probeLaunchDTO.ToString());
-
-            var highlander = probeLaunchJson["highland"].ToString().Split(' ');
-            var probe = probeLaunchJson["probe"].ToObject<List<ProbeLaunchDTO>>();
-
-            Highland highland = new Highland(int.Parse(highlander[0]), int.Parse(highlander[1]));
-
-            List<ProbeLaunchDTO> probeList = new List<ProbeLaunchDTO>();
-            foreach (var p in probe)
-            {
-                ProbeLaunchDTO pp = new ProbeLaunchDTO(
-                    p.Position_X,
-                    p.Position_Y,
-                    p.FacingDirection,
-                    highland,
-                    p.Moves
-                );
-
-                probeList.Add(pp);
-            }
-
-            var launch = new Launch(probeList);
-            return launch.ProbeLaunch();
+            var validate = new Validate(probeLaunchDTO);
+            return validate.ValidateBeforeLaunch();
         }
     }
 }
---
+++ Application/Probes/Validate.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Application;
+using Domain;
+using Newtonsoft.Json.Linq;
+
+namespace Application.Probes
+{
+    public class Validate
+    {
+        public JObject Json { get; set; }
+
+        public Validate(JObject json) {
+            Json = json;
+        }
+
+        public List<string> ValidateBeforeLaunch() {
+            var probeLaunchJson = JObject.Parse(Json.ToString());
+
+            var highlander = probeLaunchJson["highland"].ToString().Split(' ');
+            var probe = probeLaunchJson["probe"].ToObject<List<ProbeLaunchDTO>>();
+
+            Highland highland = new Highland(int.Parse(highlander[0]), int.Parse(highlander[1]));
+
+            List<ProbeLaunchDTO> probeList = new List<ProbeLaunchDTO>();
+            foreach (var p in probe)
+            {
+                ProbeLaunchDTO pp = new ProbeLaunchDTO(
+                    p.Position_X,
+                    p.Position_Y,
+                    p.FacingDirection,
+                    highland,
+                    p.Moves
+                );
+
+                probeList.Add(pp);
+            }
+
+            var launch = new Launch(probeList);
+            return launch.ProbeLaunch();
+        }
+    }
+}
